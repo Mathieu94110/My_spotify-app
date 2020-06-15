84de18daea7ecf01f8a8e5b40250e7713e0caa14
Mathieu Enault <me94110@gmail.com>
2020-06-15 18:45:38 +0200

    add little styles with material ui

--- spotify_application/src/components/home.jsx
+++ spotify_application/src/components/home.jsx
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import axios from "axios";
 import Tracks from "./tracks";
 import Search from './search';
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import { CardMedia } from "@material-ui/core";
+
+import { Button } from "@material-ui/core";
+
+
 export default class Home extends Component {
   constructor(props) {
     super(props);
@@ -40,34 +50,54 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
+      <div style={{background:"red"
+  }
+}>
         <h1>Page d'accueil</h1>
-        <div style={{ height: "500px" }}>
+        <div style={{ display: "flex", justifyContent: "center", margin:'5%' }}>
           {this.state.playlists.map((playlist, index) => {
             return (
-              <div key={index}>
-                <span style={{ fontWeight: "bold" }}>Artiste :</span>{" "}
-                {playlist.name}
-                <img src={playlist.images[0].url} style={{ width: "50px" }} />
-                <button
-                  onClick={() =>
-                    (window.location = "/tracks?id=" + playlist.id)
-                  }
-                >
-                  Voir les titres
-                </button>
-                
-              </div>
+              <Card key={index} style={{ margin: "5px" }}>
+                <CardActionArea>
+                  <CardMedia
+                    image={playlist.images[0].url}
+                    style={{ height: "200px" }}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {playlist.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() =>
+                      (window.location = "/playlists?id=" + playlist.id)
+                    }
+                  >
+                    Voir les titres
+                  </Button>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
             );
-            
-          }
-       
-          )}
-          <Search />
+          })}
         </div>
-       
+        <Search />
       </div>
-  
     );
   }
 }
--- spotify_application/src/components/search.jsx
+++ spotify_application/src/components/search.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import { Button } from "@material-ui/core";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Input } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       search: "",
+      searchActivated: false,
       tracks: [],
       albums: [],
       artists:[]
@@ -19,53 +30,117 @@ export default class Search extends Component {
     console.log(this.state.search);
   };
   addSearch() {
-    //Aut q= +keywords +type
-    let access_token = localStorage.access_token;
-    let config = {
-      headers: { Authorization: `Bearer ${access_token}` },
-      params: {
-        q: this.state.search , type: "track,album,artist"
-      },
-    };
-    axios.get("https://api.spotify.com/v1/search", config)
-      .then((res) => {
-        console.log(res.data);
-        this.setState({
-          artists: res.data.artists.items,
-          albums: res.data.albums.items,
-          tracks: res.data.tracks.items,
-        });
-      });
-  }
+                this.setState({
+                  searchActivated: true,
+                });
+                //Aut q= +keywords +type
+                let access_token = localStorage.access_token;
+                let config = {
+                  headers: { Authorization: `Bearer ${access_token}` },
+                  params: {
+                    q: this.state.search,
+                    type: "track,album,artist",
+                  },
+                };
+                axios
+                  .get("https://api.spotify.com/v1/search", config)
+                  .then((res) => {
+                    console.log(res.data);
+                    this.setState({
+                      artists: res.data.artists.items,
+                      albums: res.data.albums.items,
+                      tracks: res.data.tracks.items,
+                    });
+                  });
+              }
 
   render() {
-    return (
-      <div>
-        <div>
-          <input
-            type="text"
-            value={this.state.search}
-            onChange={this.handleChange}
-          ></input>
-         
-          <button onClick={this.addSearch.bind(this)}>Rechercher</button>
-          {this.state.tracks.map((track, index) => {
-            return <div key={index}>
-             {track.name}
-            </div>
-          })}
-          {this.state.albums.map((album, index) => {
-            return <div key={index}>
-              {album.name}
-            </div>
-          })}
-          {this.state.artists.map((artist, index) => {
-            return <div key={index}>
-              {artist.name}
-            </div>
-          })}
+    let searchActivated = this.state.searchActivated;
+    let button;
+    if (!searchActivated) {
+      return (
+        <div style={{display:"flex",justifyContent:"center"}}>
+          <form>
+            <TextField
+              id="outlined-basic"
+              label="Album, Titre ou Artiste "
+              value={this.state.search}
+              onChange={this.handleChange}
+            />
+          </form>
+
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.addSearch.bind(this)}
+          >
+            Rechercher
+          </Button>
         </div>
-      </div>
-    );
+      );
+    } else {
+       return (
+         <div>
+           <div>
+             <form>
+               <TextField
+                 id="outlined-basic"
+                 label="Album, Titre ou Artiste "
+                 value={this.state.search}
+                 onChange={this.handleChange}
+               />
+             </form>
+
+             <Button
+               variant="contained"
+               color="secondary"
+               onClick={this.addSearch.bind(this)}
+             >
+               Rechercher
+             </Button>
+
+             <div style={{ display: "flex", width: "100%" }}>
+               <div style={{ width: "30%", marginLeft: "5%" }}>
+                 <h2>Titres</h2>
+                 {this.state.tracks.map((track, index) => {
+                   return (
+                     <Card key={index} style={{ textAlign: "left" }}>
+                       <CardContent style={{ fontStyle: "bold" }}>
+                         {track.name}
+                       </CardContent>
+                     </Card>
+                   );
+                 })}
+               </div>
+               <div style={{ width: "30%" }}>
+                 <h2>Album</h2>
+                 {this.state.albums.map((album, index) => {
+                   return (
+                     <ListItem alignItems="flex-start" key={index}>
+                       <ListItemAvatar>
+                         <Avatar alt="Remy Sharp" src={album.images[2].url} />
+                       </ListItemAvatar>
+                       <ListItemText primary={album.name} />
+                     </ListItem>
+                   );
+                 })}
+               </div>
+
+               <div style={{ width: "30%", marginRight: "5%" }}>
+                 <h2>Artiste(s)</h2>
+                 {this.state.artists.map((artist, index) => {
+                   return (
+                     <Card key={index} style={{ textAlign: "left" }}>
+                       <CardContent>{artist.name}</CardContent>
+                     </Card>
+                   );
+                 })}
+               </div>
+             </div>
+           </div>
+         </div>
+       );
+    }
+   
   }
 }
