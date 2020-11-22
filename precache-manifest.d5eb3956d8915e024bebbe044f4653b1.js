ab2c968906c008d08df78e9b88c255189a2fd7ae
Mathieu Enault <me94110@gmail.com>
2020-11-22 16:31:05 +0100

    Updates

---
+++ precache-manifest.d5eb3956d8915e024bebbe044f4653b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff278478283723ef80111f32dd434f7c",
+    "url": "/My_spotify-app/index.html"
+  },
+  {
+    "revision": "1b3831f809f9545db760",
+    "url": "/My_spotify-app/static/css/main.d7e723e1.chunk.css"
+  },
+  {
+    "revision": "1f625a9d6060f5c1382a",
+    "url": "/My_spotify-app/static/js/2.b241cc30.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/My_spotify-app/static/js/2.b241cc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3831f809f9545db760",
+    "url": "/My_spotify-app/static/js/main.3b3f397e.chunk.js"
+  },
+  {
+    "revision": "ba7d5f7e5d67964f8465",
+    "url": "/My_spotify-app/static/js/runtime-main.022ecd37.js"
+  },
+  {
+    "revision": "a7757dd17fdab9e617cf8d2479d5a771",
+    "url": "/My_spotify-app/static/media/spotify_homepage_img.a7757dd1.jpg"
+  }
+]);
