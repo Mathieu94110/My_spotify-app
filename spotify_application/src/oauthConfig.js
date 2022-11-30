b78730aaad7e6f218e6929a522a99040c03c9688
MathieuEnault <enault.mathieu2@gmail.com>
2022-11-30 16:35:07 +0100

    adding scopes variables, changing spotify client id and redirect uri

--- spotify_application/src/oauthConfig.js
+++ spotify_application/src/oauthConfig.js
@@ -1,16 +1,25 @@
-const authEndpoint = "https://accounts.spotify.com/authorize";
+const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 export const getAuthorizeHref = () => {
   const scopes = [
-    "user-read-private",
-    "playlist-modify-public",
-    "playlist-modify-private",
-    "user-read-recently-played",
-    "user-top-read",
+    'user-read-private',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'user-read-recently-played',
+    'playlist-modify-private',
+    'ugc-image-upload',
+    'user-follow-modify',
+    'user-follow-read',
+    'user-library-read',
+    'user-library-modify',
+    'user-top-read',
+    'user-read-email',
+    'user-read-playback-state',
   ];
   const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
   const redirectUri = process.env.REACT_APP_REDIRECT_URI;
-  return `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-    "%20"
-  )}&response_type=token`;
+  return `${authEndpoint}?client_id=${clientId}&scope=${scopes.join(
+    '%20'
+  )}&response_type=token&redirect_uri=${redirectUri}`;
 };
