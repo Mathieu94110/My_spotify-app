86cf5be8ef752f69b2508e8a69eba9b14b0c8ed2
MathieuEnault <enault.mathieu2@gmail.com>
2022-11-30 19:54:31 +0100

    adding redirect uri for prod mode

--- spotify_application/src/oauthConfig.js
+++ spotify_application/src/oauthConfig.js
@@ -1,5 +1,5 @@
 const authEndpoint = 'https://accounts.spotify.com/authorize';
-
+import { config } from './Constants';
 export const getAuthorizeHref = () => {
   const scopes = [
     'user-read-private',
@@ -17,8 +17,12 @@ export const getAuthorizeHref = () => {
     'user-read-email',
     'user-read-playback-state',
   ];
+  const prodRedirectUri = 'https://mathieu94110.github.io/My_spotify-app/';
   const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+  const redirectUri =
+    process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_REDIRECT_URI
+      : prodRedirectUri;
   return `${authEndpoint}?client_id=${clientId}&scope=${scopes.join(
     '%20'
   )}&response_type=token&redirect_uri=${redirectUri}`;
